e7acf6cc42c91816e11f5cf77a409a89e4c9edc9
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-09-05 22:00:24 -0400

    commit tree 74a1221879ada78023ede5123e78cd72f5b95e3c

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const num3 = 22;
+const num4 = -6;
+
+const multiply = num1 * num2;
+multiply;
+
+const random = Math.floor(Math.random() * 100);
+random;
+
+const mod = num3 % num4;
+mod;
+
+const max = Math.max(...[...Array(21).keys()]);
+max;
